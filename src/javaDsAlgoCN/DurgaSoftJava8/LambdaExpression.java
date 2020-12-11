ac83f27a6490bf6fc794b91d4a2690ef70f93eff
ratnakarsharma <ratnakarsharma63@gmail.com>
2020-12-11 15:49:22 +0530

    Called function interface

--- src/javaDsAlgoCN/DurgaSoftJava8/LambdaExpression.java
+++ src/javaDsAlgoCN/DurgaSoftJava8/LambdaExpression.java
@@ -1,11 +1,15 @@
 package javaDsAlgoCN.DurgaSoftJava8;
+
 @FunctionalInterface
 interface PrintHello {
-    public void print();
+    void print();
 }
 
 public class LambdaExpression {
     public static void main(String[] args) {
-
+        PrintHello ph = ()->{
+            System.out.println("Hello");
+        };
+        ph.print();
     }
 }
