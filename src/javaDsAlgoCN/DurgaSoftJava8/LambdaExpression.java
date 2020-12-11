8118a759efebf1863c8ba8555406b901ecc0c01e
ratnakarsharma <ratnakarsharma63@gmail.com>
2020-12-11 16:22:33 +0530

    Lambda expression and square method

--- src/javaDsAlgoCN/DurgaSoftJava8/LambdaExpression.java
+++ src/javaDsAlgoCN/DurgaSoftJava8/LambdaExpression.java
@@ -1,15 +1,26 @@
 package javaDsAlgoCN.DurgaSoftJava8;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @FunctionalInterface
-interface PrintHello {
-    void print();
+interface Square {
+    List<Integer> square();
 }
 
 public class LambdaExpression {
     public static void main(String[] args) {
-        PrintHello ph = ()->{
-            System.out.println("Hello");
-        };
-        ph.print();
+        List<Integer> list = new ArrayList<>();
+        List<Integer> ansL = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i+10);
+        }
+        Square s = ()-> list
+               .stream()
+               .filter(ele -> ele%2 ==0)
+               .collect(Collectors.toList());
+        ansL = s.square();
+        System.out.println(ansL);
     }
 }
